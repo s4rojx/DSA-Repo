bed4698608c56472ec238c0cfbb9c2354368c670
Saroj Ghosh <saroj29.ghosh@gmail.com>
2026-03-13 16:42:40 +0530

    Enhance comments in ClimbStairsVJumps.java
    Added comments to clarify the logic for dynamic programming approach.

--- DynamicProgramming/src/ClimbStairsVJumps.java
+++ DynamicProgramming/src/ClimbStairsVJumps.java
@@ -9,10 +9,14 @@ public class ClimbStairsVJumps {
             arr[i] = sc.nextInt();
         }
         int [] dp = new int[n+1];
-        dp[n] = 1;
+        dp[n] = 1;//n se n jaane ka ek hi raasta hota hai 
+      //dp[i] pe , i se n tak ke jaane ke raste saved hain
         //Here we move from right to left so i loop runs ulta
-        for (int i = n-1; i >=0; i--) {
-            for (int j = 1; j <= arr[i] && i+j < dp.length; j++) {
+        for (int i = n-1; i >=0; i--)//dp[i] pe store karega ki i se n jaane ka kitna rasta hota hai
+           {//arr[i] contains in how may ways can we go from i to n 
+            for (int j = 1; j <= arr[i] && i+j < dp.length; j++) 
+            //(i+j) condtition is given to preven going beyond the dp array
+            {
                 dp[i] += dp[i+j];
             }
         }
