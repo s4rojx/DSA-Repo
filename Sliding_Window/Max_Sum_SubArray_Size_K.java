09af3e46ca9b9efbde65d4c41632843a556bc6dd
Saroj Ghosh <saroj29.ghosh@gmail.com>
2026-03-23 22:38:58 +0530

    code for 23-03-26

---
+++ Sliding_Window/Max_Sum_SubArray_Size_K.java
@@ -0,0 +1,28 @@
+package Sliding_Window;
+
+import java.util.Scanner;
+
+public class Max_Sum_SubArray_Size_K {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        int sum = 0 ;
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+        int mSum = sum;
+        for (int i = 0; i < n - k; i++) {
+            sum = sum - arr[i] + arr[i+k];
+            if(sum > mSum){
+                mSum = sum;
+            }
+        }
+        System.out.println(mSum);
+
+    }
+}
