7f9cdb410117861e536033edfe1355a3490ab4e9
Saroj Ghosh <saroj29.ghosh@gmail.com>
2026-03-26 23:43:44 +0530

    Added comment

--- Sliding_Window/Max_Sum_SubArray_Size_K.java
+++ Sliding_Window/Max_Sum_SubArray_Size_K.java
@@ -10,11 +10,14 @@ public class Max_Sum_SubArray_Size_K {
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
+        //
         int k = sc.nextInt();
         int sum = 0 ;
         for (int i = 0; i < k; i++) {
             sum += arr[i];
         }
+
+        //Complexity O(N)
         int mSum = sum;
         for (int i = 0; i < n - k; i++) {
             sum = sum - arr[i] + arr[i+k];
