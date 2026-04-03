491830fb604d8fc2be36a2ca85da725e830be5ab
Saroj Ghosh <saroj29.ghosh@gmail.com>
2026-04-03 17:17:11 +0530

    Buy and sell stocks codes added

---
+++ Buy_And_Sell_Stocks/Buy_And_Sell_Stocks_III.java
@@ -0,0 +1,46 @@
+package PACKAGE_NAME;
+import java.io.*;
+import java.util.*;
+
+public class Buy_And_Sell_Stocks_III {
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+
+        String nextLine() {
+            String str = "";
+            try { str = br.readLine(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        FastScanner sc = new FastScanner();
+        StringBuilder out = new StringBuilder();
+       
+        //Code
+       
+        System.out.print(out);
+    }
+}
